19f14ad868c4589d7145d799c064155dbb9c07f4
alwinabraham <alwinabraham098@gmail.com>
2023-02-08 17:45:44 +0530

    error handled

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ io.on('connection', socket =>{
     })
     // socket.emit('chat-message','Hello World')
     socket.on('send-chat-message',message =>{
-        // console.log(message);
         socket.broadcast.emit('chat-message',{message:message,name:users[socket.id]})
     })
     socket.on('disconnect', () =>{
